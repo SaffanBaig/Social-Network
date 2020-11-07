2eea7b1b28dd6347984f30d5c90d7bfc01fb44ef
Saffan Baig <mirzasaffan492@gmail.com>
2020-11-07 13:05:16 +0500

    Setup Routes

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async() => {
+  try{
+    await mongoose.connect(db,{ 
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MONGO DB CONNECTED');
+  }catch(err){
+    console.error(err.message);
+    // EXIT PROCESS with FAILURE
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
